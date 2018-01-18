b20b433e1e16f06ffa44c99e4f4c52023ffd5383
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2018-01-19 00:45:07 +0100

    Working plugin, can handle pos and rot. Behavior not 100% yet

--- src/wire_plugin.cpp
+++ src/wire_plugin.cpp
@@ -7,6 +7,8 @@
 #include <ros/node_handle.h>
 #include <ros/advertise_service_options.h>
 
+#include <geolib/datatypes.h>
+
 //msgs
 #include <wire_msgs/WorldEvidence.h>
 
@@ -34,7 +36,7 @@ void WirePlugin::configure(tue::Configuration config)
     ros::NodeHandle nh("~");
 //    ros::NodeHandle nh_private;
     ros::SubscribeOptions sub_options = ros::SubscribeOptions::create<wire_msgs::WorldState>(
-                    state_topic, 10, boost::bind(&WirePlugin::wireCallback, this, _1), ros::VoidPtr(), &cb_queue_);
+                    state_topic, 1, boost::bind(&WirePlugin::wireCallback, this, _1), ros::VoidPtr(), &cb_queue_);
     wire_sub_ = nh.subscribe(sub_options);
 }
 
@@ -58,10 +60,93 @@ void WirePlugin::process(const ed::WorldModel& world, ed::UpdateRequest& req)
 
 void WirePlugin::wireCallback(const wire_msgs::WorldStateConstPtr& msg)
 {
-    ROS_ERROR("[ED WIRE] Callback");
+    // processing all objects from wire
     for(std::vector<wire_msgs::ObjectState>::const_iterator it = msg->objects.begin(); it != msg->objects.end(); ++it)
     {
-        ROS_ERROR_STREAM("ID: " << it->ID);
+        std::string ed_id = "wire-"+std::to_string(it->ID);
+
+        const ed::EntityConstPtr e = world_model_->getEntity(ed_id);
+
+        geo::Pose3D pose = geo::Pose3D::identity();
+
+        // Check if the entity is already in the world_model, because could also be a new entity
+        if(e)
+        {
+            if(e->has_pose())
+                pose = e->pose();
+        }
+
+        bool updated = false;
+        for(std::vector<wire_msgs::Property>::const_iterator it2 = it->properties.begin(); it2 != it->properties.end(); ++it2)
+        {
+            if(it2->attribute == "position")
+            {
+                if(it2->pdf.dimensions != 3)
+                {
+                    ROS_ERROR_STREAM("[ED WIRE] Position of ID: '" << it->ID << "' is not of dimension '3'");
+                    continue;
+                }
+                const double& x = it2->pdf.data[0];
+                const double& y = it2->pdf.data[1];
+                const double& z = it2->pdf.data[2];
+                pose.setOrigin(geo::Vec3(x, y, z));
+                updated = true;
+            }
+            else if(it2->attribute == "orientation")
+            {
+                if(it2->pdf.dimensions != 4)
+                {
+                    ROS_ERROR_STREAM("[ED WIRE] Orientation of ID: '" << it->ID << "' is not of dimension '4'");
+                    continue;
+                }
+                const double& r = it2->pdf.data[0];
+                const double& p = it2->pdf.data[1];
+                const double& y = it2->pdf.data[2];
+                const double& w = it2->pdf.data[3];
+                pose.R.setRotation(geo::Quaternion(r, p, y, w));
+                updated = true;
+            }
+        }
+
+        // Only set pose when position or orientation is updated with a correct measurement.
+        // When there is no correct measurement of position OR orientation and entity is not already in the world_model it is not added.
+        // TODO: New entities should at least have a correct position, before being allowed to be added to world_model.
+        if(updated)
+            update_req_->setPose(ed_id, pose);
+
+    }
+
+    // Removing all entities, which originate from wire, which are not in wire anymore. So keeping entities with incorrect measurements
+    // TODO: should we keep entities with incorrect measurements?
+    for(ed::WorldModel::const_iterator it = world_model_->begin(); it != world_model_->end(); ++it)
+    {
+        const ed::EntityConstPtr& e = *it;
+
+        // Ignore all entities that are not originating from wire
+        if(e->id().str().find("wire") == std::string::npos)
+            continue;
+
+        // Check if id is in last wire state
+        for(std::vector<wire_msgs::ObjectState>::const_iterator it = msg->objects.begin(); it != msg->objects.end(); ++it)
+        {
+            std::string ed_id = "wire-"+std::to_string(it->ID);
+            if(e->id().str() == ed_id)
+               break;
+
+            ROS_DEBUG_STREAM("[ED WIRE] Entity: '" << e->id().str() << "'is not equal to '" << ed_id << "'");
+            //Only remove when not equal and all objects in wire_state checked
+            if(std::next(it) == msg->objects.end())
+            {
+                ROS_DEBUG_STREAM("[ED WIRE] Removing entity: '" << e->id().str() << "', because it is not in wire anymore");
+                update_req_->removeEntity(e->id());
+            }
+        }
+
+       // This would also remove entities with incorrect measurements, which should be bad if it only happens
+//        if(update_req_->updated_entities.count(e->id()) == 0) // TODO: ?Or should it be != 1?
+//        {
+//            update_req_->removeEntity(e->id());
+//        }
     }
 }
 
